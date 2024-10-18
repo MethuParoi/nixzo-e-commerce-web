740961fd75f7d64624d9ac3dd05fbcf11a3fa389
MethuParoi <paroimethu@gmail.com>
2024-10-18 22:13:43 +0600

    signin issue fix-07

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -35,7 +35,8 @@ export async function GET(request: Request) {
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  // return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_REDIRECT_URL}`);
 }
 
 //-------------------------------------------
