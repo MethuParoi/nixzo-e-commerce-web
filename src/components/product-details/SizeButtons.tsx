977374d000e4f33796ea7e2e1757577d4ea30826
MethuParoi <paroimethu@gmail.com>
2024-11-05 12:37:22 +0600

    sbf-02

--- src/components/product-details/SizeButtons.tsx
+++ src/components/product-details/SizeButtons.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import Button from "../ui/Button";
 
 function SizeButtons({ setSelectedSize, selectedSize }) {
@@ -17,6 +17,7 @@ function SizeButtons({ setSelectedSize, selectedSize }) {
     if (setSelectedSize) {
       setSelectedSize(label); // Update the selected size
     }
+    // console.log("Size selected:", label);
   };
 
   return (
@@ -26,94 +27,38 @@ function SizeButtons({ setSelectedSize, selectedSize }) {
         type="size"
         onClick={() => handleClick("M")}
         isActive={activeButton === "M"}
+        setActiveButton={setSelectedSize}
       />
       <Button
         label="L"
         type="size"
         onClick={() => handleClick("L")}
         isActive={activeButton === "L"}
+        setActiveButton={setSelectedSize}
       />
       <Button
         label="XL"
         type="size"
-        onClick={() => handleClick("XL")}
+        onClick={() => {
+          handleClick("XL");
+        }}
         isActive={activeButton === "XL"}
+        setActiveButton={setSelectedSize}
       />
       <Button
         label="XXL"
         type="size"
-        onClick={() => handleClick("XXL")}
+        onClick={() => {
+          handleClick("XXL");
+        }}
         isActive={activeButton === "XXL"}
+        setActiveButton={setSelectedSize}
       />
     </div>
   );
 }
 
 export default SizeButtons;
-
-//----------------------------------------
-
-// "use client";
-
-// import React, { use, useEffect, useState } from "react";
-// import Button from "../ui/Button";
-
-// function SizeButtons({ setSelectedSize, selectedSize }) {
-//   const [activeButton, setActiveButton] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     if (selectedSize) {
-//       setActiveButton(selectedSize);
-//     }
-//   }, [selectedSize]);
-
-//   const handleClick = (label: string) => {
-//     setActiveButton(label);
-//     if (setSelectedSize) {
-//       setSelectedSize(label); // Update the selected size
-//     }
-//     // console.log("Size selected:", label);
-//   };
-
-//   return (
-//     <div className="flex items-center gap-x-5">
-//       <Button
-//         label="M"
-//         type="size"
-//         onClick={() => handleClick("M")}
-//         isActive={activeButton === "M"}
-//         setActiveButton={undefined}
-//       />
-//       <Button
-//         label="L"
-//         type="size"
-//         onClick={() => handleClick("L")}
-//         isActive={activeButton === "L"}
-//         setActiveButton={undefined}
-//       />
-//       <Button
-//         label="XL"
-//         type="size"
-//         onClick={() => {
-//           handleClick("XL");
-//         }}
-//         isActive={activeButton === "XL"}
-//         setActiveButton={undefined}
-//       />
-//       <Button
-//         label="XXL"
-//         type="size"
-//         onClick={() => {
-//           handleClick("XXL");
-//         }}
-//         isActive={activeButton === "XXL"}
-//         setActiveButton={undefined}
-//       />
-//     </div>
-//   );
-// }
-
-// export default SizeButtons;
 
 //--------------------------------------------
 
